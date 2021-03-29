e39e34d62a6f4084fb403c344ee5b9d4da83c7f0
k-joseph <kaweesi@jembi.org>
2021-03-29 11:40:15 +0300

    mail & cache fix

--- telefynaConfiguration/cache.php
+++ telefynaConfiguration/cache.php
@@ -1,4 +1,4 @@
 <?php
-    $metrics = "Disk Usage: ".ceil((disk_free_space("/")/disk_total_space("/")) * 100)."%\r\n";
-    file_put_contents("exports/".$now.".txt", $metrics.json_encode($_POST, JSON_PRETTY_PRINT)."\r\n______________________________________________________\r\n");
+    $now = date('Y-m-d_H:i:s');
+    file_put_contents("exports/".$now.".txt", json_encode($_POST, JSON_PRETTY_PRINT)."\r\n______________________________________________________\r\n");
 ?>
--- telefynaConfiguration/sendMail.sh
+++ telefynaConfiguration/sendMail.sh
@@ -3,8 +3,16 @@
 cd /var/www/html/telefynaConfiguration/;
 FILES=exports/$(date '+%Y-%m-%d')*.txt
 if compgen -G "$FILES" > /dev/null; then
-    cat mailHeader.txt exports/$(date '+%Y-%m-%d')*.txt mailFooter.txt > content.txt;
+    echo "DiskUsage:__________________________" > metrics.txt
+    df -H >> metrics.txt
+    echo "CPUUsage:__________________________" >> metrics.txt
+    mpstat >> metrics.txt
+    echo "Uptime:__________________________" >> metrics.txt
+    uptime | awk -F'( |,|:)+' '{print $6,$7",",$8,"hours,",$9,"minutes."}' >> metrics.txt
+
+    cat mailHeader.txt metrics.txt exports/$(date '+%Y-%m-%d')*.txt mailFooter.txt > content.txt;
     cat content.txt | msmtp --debug -a gmail apps@avventohome.org;
+    rm metrics.txt
     rm content.txt;
 fi
 cd -;
